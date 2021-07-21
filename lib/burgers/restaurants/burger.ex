cc98dca0f9785f3020fa656612424eb2c41b9c6a
Lise <lise.carpenter@gmail.com>
2021-07-21 16:05:43 +0700

    WIP

--- lib/burgers/restaurants/burger.ex
+++ lib/burgers/restaurants/burger.ex
@@ -6,6 +6,23 @@ defmodule Burgers.Restaurants.Burger do
     use Burgers.Storage.Base, module: Burgers.Restaurants.Burger
   end
 
+  @spec new(%{
+          :delivery => any,
+          :location => any,
+          :name => any,
+          :price => any,
+          :ranking => any,
+          :tags => any,
+          optional(any) => any
+        }) :: %Burgers.Restaurants.Burger{
+          delivery: any,
+          id: binary,
+          location: any,
+          name: any,
+          price: any,
+          ranking: any,
+          tags: any
+        }
   def new(%{name: name, location: location, price: price, ranking: ranking, delivery: delivery, tags: tags}) do
     %__MODULE__{
       id: UUID.uuid4(),
