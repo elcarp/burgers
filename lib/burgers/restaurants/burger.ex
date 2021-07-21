8cca8178b30ef06e3c18db95070a02443db503d1
Lise <lise.carpenter@gmail.com>
2021-07-21 16:06:20 +0700

    remove extra data points for now

--- lib/burgers/restaurants/burger.ex
+++ lib/burgers/restaurants/burger.ex
@@ -1,37 +1,16 @@
 defmodule Burgers.Restaurants.Burger do
 
-  defstruct [:id, :name, :location, :price, :ranking, :delivery, :tags]
+  defstruct [:id, :name, :location]
 
   defmodule Store do
     use Burgers.Storage.Base, module: Burgers.Restaurants.Burger
   end
 
-  @spec new(%{
-          :delivery => any,
-          :location => any,
-          :name => any,
-          :price => any,
-          :ranking => any,
-          :tags => any,
-          optional(any) => any
-        }) :: %Burgers.Restaurants.Burger{
-          delivery: any,
-          id: binary,
-          location: any,
-          name: any,
-          price: any,
-          ranking: any,
-          tags: any
-        }
-  def new(%{name: name, location: location, price: price, ranking: ranking, delivery: delivery, tags: tags}) do
+  def new(%{name: name, location: location}) do
     %__MODULE__{
       id: UUID.uuid4(),
       name: name,
       location: location,
-      price: price,
-      ranking: ranking,
-      delivery: delivery,
-      tags: tags
     }
   end
 end
