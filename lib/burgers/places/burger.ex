51d2042f2587c776b42e55c302a7c4bf054523ec
Lise <lise.ca@deemoney.com>
2021-08-04 11:30:47 +0700

    add food and branches

--- lib/burgers/places/burger.ex
+++ lib/burgers/places/burger.ex
@@ -1,18 +1,19 @@
 defmodule Burgers.Places.Burger do
   @derive Jason.Encoder
 
-  defstruct [:id, :name, :location]
+  defstruct [:id, :name, :location, :food, :branch]
 
   defmodule Store do
     use Burgers.Storage.Base, module: Burgers.Places.Burger
   end
-  #brand / branch/ food
 
-  def new(%{name: name, location: location}) do
+  def new(%{name: name, location: location, food: food, branch: branch}) do
     %__MODULE__{
       id: UUID.uuid4(),
       name: name,
-      location: location
+      location: location,
+      food: food,
+      branch: branch
     }
   end
 end
