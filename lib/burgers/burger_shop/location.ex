da058bf6fddd15cf2c8f99d6b95b54204f4bb420
Lise <lise.carpenter@gmail.com>
2021-08-04 15:35:57 +0700

    location

--- lib/burgers/burger_shop/location.ex
+++ lib/burgers/burger_shop/location.ex
@@ -1,17 +1,16 @@
-alias Burgers.Storage.Association
-
 defmodule Burgers.BurgerShop.Location do
-  defstruct [:id, :location, :burger, :lat, :long, :delivery]
+  defstruct [:id, :lat, :long, :delivery]
 
   defmodule Store do
     use Burgers.Storage.Base, module: Burgers.BurgerShop.Location
   end
 #many to many?
-  def new(%{location: location, burger: burger}) do
+  def new(%{lat: lat, long: long, delivery: delivery}) do
     %__MODULE__{
       id: UUID.uuid4(),
-      location: location,
-      burger: Association.new(location)
+      lat: lat,
+      long: long,
+      delivery: delivery
     }
   end
 end
