614d6335ea3987c815046669352329d04afeab42
Lise <lise.ca@deemoney.com>
2021-09-10 13:33:07 +0700

    format

---
+++ lib/burgers/burger_shop/location.ex
@@ -0,0 +1,17 @@
+alias Burgers.Storage.Association
+
+defmodule Burgers.BurgerShop.Location do
+  defstruct [:id, :location, :burger, :lat, :long, :delivery]
+
+  defmodule Store do
+    use Burgers.Storage.Base, module: Burgers.BurgerShop.Location
+  end
+
+  def new(%{location: location, burger: burger}) do
+    %__MODULE__{
+      id: UUID.uuid4(),
+      location: location,
+      burger: Association.new(location)
+    }
+  end
+end
